95a5d6aca35370d362a78d037157d9b662592f33
Maicon Cubero <maiconcubero@outlook.com>
2026-03-20 19:14:55 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({   origin: '*',   methods: ['GET', 'POST'],   allowedHeaders: ['Content-Type'] }));
 app.use(express.json());
 
 // Health check
